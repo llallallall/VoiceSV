c97b468d2b73dc5b30b50d467095652f31a6ca2b
llallallall <llallallall@218.38.137.28>
2021-09-05 08:52:56 +0900

    initial commit

---
+++ src/main/java/com/project/redowls/VoiceSvApplication.java
@@ -0,0 +1,13 @@
+package com.project.redowls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceSvApplication.class, args);
+	}
+
+}
